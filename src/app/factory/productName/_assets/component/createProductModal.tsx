31c1a429ac3896881518c194acdf94b042c501b5
naimur61 <m.naimur61@gmail.com>
2025-12-13 02:59:56 +0600

    fix

--- src/app/factory/productName/_assets/component/createProductModal.tsx
+++ src/app/factory/productName/_assets/component/createProductModal.tsx
@@ -2,7 +2,9 @@ import { useApiMutation } from "@/app/utils/TanstackQueries/useApiMutation";
 import ActionButton from "@/components/common/button/actionButton";
 import { DialogWrapper } from "@/components/common/common_dialog/common_dialog";
 import { CustomField } from "@/components/common/fields/cusField";
+import { showToast } from "@/components/common/TostMessage/customTostMessage";
 import { Label } from "@/components/ui/label";
+import { useQueryClient } from "@tanstack/react-query";
 import { PlusIcon } from "lucide-react";
 import React from "react";
 
@@ -10,12 +12,15 @@ const CreateProductNameModal = ({ factoryId }: { factoryId: string }) => {
   const [open, setOpen] = React.useState(false);
   const [pName, setPName] = React.useState("");
   const [pType, setPType] = React.useState("");
+  const queryClient = useQueryClient();
 
   const addProductName = useApiMutation({
     path: "factory/category",
     method: "POST",
-    onSuccess: () => {
+    onSuccess: (data) => {
       setOpen(false);
+      showToast("success", data);
+      queryClient.invalidateQueries({ queryKey: ["getProductNameData"] });
     },
   });
 
@@ -62,6 +67,7 @@ const CreateProductNameModal = ({ factoryId }: { factoryId: string }) => {
 
         <div className="flex justify-end mt-4">
           <ActionButton
+            isPending={addProductName.isPending}
             buttonContent="Add"
             btnStyle="w-full bg-blue-500 text-white"
             handleOpen={() => handleAddProductName()}
